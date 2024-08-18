df4cb95ce7a6ee91c66475aeb919ce219a130495
clever <clever@cleverdeMacBook-Pro.local>
2024-08-18 20:28:01 +0800

    feat:support concurrent request

--- app/server.go
+++ app/server.go
@@ -8,20 +8,23 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go serve(conn)
 	}
+
+}
+
+func serve(conn net.Conn) {
 	receiveData := make([]byte, 1024)
 	length, err := conn.Read(receiveData)
 	request := string(receiveData[:length])
@@ -40,7 +43,6 @@ func main() {
 		header := strings.Split(requestSegments[i], ": ")
 		headers[header[0]] = header[1]
 	}
-
 	fmt.Sprintln(requestSegments)
 	if path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
