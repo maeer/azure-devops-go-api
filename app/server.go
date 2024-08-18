a426185bb073c586b18fc85805b0627713e7d513
clever <clever@cleverdeMacBook-Pro.local>
2024-08-18 20:22:13 +0800

    feat:parse User-Agent header

--- app/server.go
+++ app/server.go
@@ -30,11 +30,25 @@ func main() {
 		fmt.Println("Error reading: ", err.Error())
 		os.Exit(1)
 	}
+	requestSegments := strings.Split(request, "\r\n")
 	path := strings.Split(request, " ")[1]
+	headers := map[string]string{}
+	for i := 1; i < len(requestSegments); i++ {
+		if requestSegments[i] == "" {
+			break
+		}
+		header := strings.Split(requestSegments[i], ": ")
+		headers[header[0]] = header[1]
+	}
+
+	fmt.Sprintln(requestSegments)
 	if path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.Split(path, "/")[1] == "echo" {
-		message := strings.Split(path, "/")[2]
+	} else if segments := strings.Split(path, "/"); segments[1] == "echo" {
+		message := segments[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+	} else if segments[1] == "user-agent" {
+		message := headers["User-Agent"]
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
